5bd0e73ce847984718c56abee6f3ce76dc13b404
famaurel <fannymaurel@outlook.com>
2021-10-13 17:31:51 +0200

    fix mistake in model: article

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,5 @@
 class Article < ApplicationRecord
   belongs_to :category
   has_many :comments, dependent: :destroy
-  has_many :attached_images, dependent: :destroy
+  has_many_attached :images, dependent: :destroy
 end
