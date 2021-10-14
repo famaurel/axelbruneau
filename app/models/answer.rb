40cbf0096a4df49c11510af017e16e8d9da0ae29
famaurel <fannymaurel@outlook.com>
2021-10-14 19:11:16 +0200

    started styling comment section

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,4 +1,4 @@
 class Answer < ApplicationRecord
-  has_one :user
+  belongs_to :user
   belongs_to :comment
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,5 @@
 class Comment < ApplicationRecord
   belongs_to :article
-  has_one :user
+  belongs_to :user
   has_many :answers, dependent: :destroy
 end
