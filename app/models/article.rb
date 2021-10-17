c0001014dad9a1cbbd130601152b0420a23efc44
famaurel <fannymaurel@outlook.com>
2021-10-17 21:30:25 +0200

    forms + devise front

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,6 +2,5 @@ class Article < ApplicationRecord
   belongs_to :category
   has_many :comments, dependent: :destroy
   has_many :answers, :through => :comments
-  has_many_attached :images, dependent: :destroy
   has_rich_text :content
 end
