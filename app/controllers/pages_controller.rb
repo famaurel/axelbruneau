f5aea1ea6599a9d757ba4da89d03f432b91af8eb
famaurel <fannymaurel@outlook.com>
2021-10-18 18:32:33 +0200

    user/admin authentication set

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,3 +1,4 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:mentions, :presentation, :projet]
+  skip_before_action :authenticate_user!
+  # , only: [:mentions, :presentation, :projet]
 end
