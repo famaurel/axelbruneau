0dd1c809e75ba4aabcf086998123b6ea4a93b9f5
famaurel <fannymaurel@outlook.com>
2021-10-19 16:28:18 +0200

    setup mailer in production and make the trix editor bigger

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,6 +64,22 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+  host = 'https://auxvillageslarepublique.herokuapp.com/' #replace with your own url
+  config.action_mailer.default_url_options = { :host => "https://auxvillageslarepublique.herokuapp.com/" } #=> Or https://www.example.com
+
+  config.action_mailer.perform_deliveries = true
+
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               'herokuapp.com',
+    user_name:            Rails.application.credentials.gmail[:user_name],
+    password:             Rails.application.credentials.gmail[:user_password],
+    authentication:       'plain',
+    enable_starttls_auto: true }
+
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
