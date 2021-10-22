a3d27556a95af549830c6c52b3194df51bb0a31e
famaurel <fannymaurel@outlook.com>
2021-10-22 16:00:34 +0200

    add aws credentials

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -31,7 +31,7 @@ Rails.application.configure do
   end
 
   # Store uploaded files on the local file system (see config/storage.yml for options).
-  config.active_storage.service = :local
+  config.active_storage.service = :amazon
 
   config.action_mailer.delivery_method = :smtp
   host = 'localhost:3000' #replace with your own url
